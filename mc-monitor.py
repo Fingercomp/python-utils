f03778977ae6590c36451c81cca8b2b5bb1167fa
Fingercomp <fingercomp@users.noreply.github.com>
2016-06-02 16:12:47 +0600

    Finally removed that notification thing.
    This is kind of the end.
    
    I supported them for a very long time, but now they don't need me.
    
    The community there is awesome. But the administrators... Do they even think of things they do?..
    
    Eh, I don't need answers on these questions anymore.

--- mc-monitor.py
+++ mc-monitor.py
@@ -58,9 +58,9 @@ if not os.path.exists(config + "mc-monitor/mc-monitor.cfg"):
     dialog.run()
     dialog.destroy()
 
-if not os.path.exists(config + "mc-monitor/vote"):
-    f = open(config + "mc-monitor/vote", "w")
-    f.write("2012 12 12 12 12 12")
+#if not os.path.exists(config + "mc-monitor/vote"):
+#    f = open(config + "mc-monitor/vote", "w")
+#    f.write("2012 12 12 12 12 12")
 
 if not os.path.exists(config + "icons/"):
     os.mkdir(config + "icons/")
@@ -73,26 +73,26 @@ if not os.path.exists(config + "icons/mc-monitor.png"):
     f.write(img)
     f.close()
 
-if not os.path.exists(config + "icons/mc-monitor-important.png"):
-    response = ur.urlopen("https://raw.githubusercontent.com/Fingercomp/" \
-        "python-utils/master/icons/mc-monitor-important.png")
-    f = open(config + "icons/mc-monitor-important.png", "wb")
-    img = response.read()
-    f.write(img)
-    f.close()
+#if not os.path.exists(config + "icons/mc-monitor-important.png"):
+#    response = ur.urlopen("https://raw.githubusercontent.com/Fingercomp/" \
+#        "python-utils/master/icons/mc-monitor-important.png")
+#    f = open(config + "icons/mc-monitor-important.png", "wb")
+#    img = response.read()
+#    f.write(img)
+#    f.close()
 
 DELAY = 15
-VOTEFILE = config + "mc-monitor/vote"
+#VOTEFILE = config + "mc-monitor/vote"
 SERVERSFILE = config + "mc-monitor/mc-monitor.cfg"
 
-nots = True
+#nots = True
 
-if os.name == "nt":
-    # Windows, turn off notifications
-    nots = False
+#if os.name == "nt":
+#    # Windows, turn off notifications
+#    nots = False
 
-if nots:
-    from gi.repository import Notify
+#if nots:
+#    from gi.repository import Notify
 
 # http://stackoverflow.com/a/13151299
 class RepeatedTimer(object):
@@ -137,17 +137,17 @@ class CheckServers:
 
         self.menu_setup()
         self.ind.connect("popup-menu", lambda icon, btn, time: self.menu.popup(None, None, None, None, btn, time))
-        if nots is True:
-            Notify.init("check-servers")
-            self.notification = Notify.Notification.new("Vote?", "It's time to vote!")
-            self.min30_sent = False
-            self.min15_sent = False
-            self.min5_sent = False
-            self.min1_sent = False
-        self.show_notification = False
+#        if nots is True:
+#            Notify.init("check-servers")
+#            self.notification = Notify.Notification.new("Vote?", "It's time to vote!")
+#            self.min30_sent = False
+#            self.min15_sent = False
+#            self.min5_sent = False
+#            self.min1_sent = False
+#        self.show_notification = False
         self.ready_to_show = True
         self.gui_upd = False
-        self.cur_icon = 0
+#        self.cur_icon = 0
 
     def menu_setup(self):
         self.menu = Gtk.Menu()
@@ -161,14 +161,14 @@ class CheckServers:
             self.menu.append(cws[0])
         
         
-        self.separator_vote = Gtk.SeparatorMenuItem()
-        self.separator_vote.show()
-        self.menu.append(self.separator_vote)
+#        self.separator_vote = Gtk.SeparatorMenuItem()
+#        self.separator_vote.show()
+#        self.menu.append(self.separator_vote)
         
-        self.vote_item = Gtk.MenuItem("Loading...")
-        self.vote_item.connect("activate", self.rewrite_date)
-        self.vote_item.show()
-        self.menu.append(self.vote_item)
+#        self.vote_item = Gtk.MenuItem("Loading...")
+#        self.vote_item.connect("activate", self.rewrite_date)
+#        self.vote_item.show()
+#        self.menu.append(self.vote_item)
 
 
         self.separator_controls = Gtk.SeparatorMenuItem()
@@ -191,7 +191,7 @@ class CheckServers:
         self.upddata_timer = RepeatedTimer(DELAY, self.update_data)
         self.upddata_timer.start()
         GLib.timeout_add(1000, self.update)
-        GLib.timeout_add(1000, self.update_vote)
+#        GLib.timeout_add(1000, self.update_vote)
         Gtk.main()
     
     def spawn_upddata_thread(self, widget=True):
@@ -200,71 +200,71 @@ class CheckServers:
 
     def quit(self, widget=True):
         self.upddata_timer.cancel()
-        if nots:
-            Notify.uninit()
+#        if nots:
+#            Notify.uninit()
         Gtk.main_quit()
         
-    def Notify_vote(self):
-        if self.show_notification is True and nots is True:
-            self.notification.show()
-            return True
-        return False
+#    def Notify_vote(self):
+#        if self.show_notification is True and nots is True:
+#            self.notification.show()
+#            return True
+#        return False
     
-    def rewrite_date(self, widget=True):
-        cur_time = dt.datetime.now()
-        open(VOTEFILE, "w").write((cur_time + dt.timedelta(days=1)).strftime("%Y %m %d %H %M %S"))
+#    def rewrite_date(self, widget=True):
+#        cur_time = dt.datetime.now()
+#        open(VOTEFILE, "w").write((cur_time + dt.timedelta(days=1)).strftime("%Y %m %d %H %M %S"))
     
-    def update_vote(self):
-        vote_at = dt.datetime(*[int(i) for i in open(VOTEFILE).read().strip().split(" ")])
-        cur_time = dt.datetime.now()
-        if cur_time >= vote_at:
-            self.vote_item.set_sensitive(True)
-            if self.cur_icon != 1:
-                self.ind.set_from_file(config + "icons/mc-monitor-important.png")
-                self.cur_icon = 1
-            self.vote_item.set_label("Restart the timer")
-            if self.show_notification is False and nots is True:
-                self.show_notification = True
-                self.notification.show()
-                GLib.timeout_add(30000, self.Notify_vote)
-        else:
-            if self.cur_icon != 0:
-                self.ind.set_from_file(config + "icons/mc-monitor.png")
-                self.cur_icon = 0
-            vote_delta = vote_at - cur_time
-            self.vote_item.set_label("Next vote: " + str(vote_delta).split(".")[0])
-            self.vote_item.set_sensitive(False)
-            self.show_notification = False
-            if nots is True:
-                if vote_delta.seconds < 40:
-                    # Reset values to Notify again next time
-                    self.min30_sent = False
-                    self.min15_sent = False
-                    self.min5_sent = False
-                    self.min1_sent = False
-                elif vote_delta.seconds > 55 and vote_delta.seconds < 60 and self.min1_sent is False:
-                    # Send a notification: 1 minute
-                    Notify.Notification.new("Prepare to vote", "1 minute left!").show()
-                    self.min30_sent = True
-                    self.min15_sent = True
-                    self.min5_sent = True
-                    self.min1_sent = True
-                elif vote_delta.seconds > 295 and vote_delta.seconds < 300 and self.min5_sent is False:
-                    # Send a notification: 5 minutes
-                    Notify.Notification.new("Prepare to vote", "5 minutes left!").show()
-                    self.min30_sent = True
-                    self.min15_sent = True
-                    self.min5_sent = True
-                elif vote_delta.seconds > 895 and vote_delta.seconds < 900 and self.min15_sent is False:
-                    # Send a notification: 15 minutes
-                    Notify.Notification.new("Prepare to vote", "15 minutes left!").show()
-                    self.min30_sent = True
-                    self.min15_sent = True
-                elif vote_delta.seconds > 1795 and vote_delta.seconds < 1800 and self.min30_sent is False:
-                    # Send a notification: 30 minutes
-                    Notify.Notification.new("Prepare to vote", "30 minutes left!").show()
-                    self.min30_sent = True
-        return True
+#    def update_vote(self):
+#        vote_at = dt.datetime(*[int(i) for i in open(VOTEFILE).read().strip().split(" ")])
+#        cur_time = dt.datetime.now()
+#        if cur_time >= vote_at:
+#            self.vote_item.set_sensitive(True)
+#            if self.cur_icon != 1:
+#                self.ind.set_from_file(config + "icons/mc-monitor-important.png")
+#                self.cur_icon = 1
+#            self.vote_item.set_label("Restart the timer")
+#            if self.show_notification is False and nots is True:
+#                self.show_notification = True
+#                self.notification.show()
+#                GLib.timeout_add(30000, self.Notify_vote)
+#        else:
+#            if self.cur_icon != 0:
+#                self.ind.set_from_file(config + "icons/mc-monitor.png")
+#                self.cur_icon = 0
+#            vote_delta = vote_at - cur_time
+#            self.vote_item.set_label("Next vote: " + str(vote_delta).split(".")[0])
+#            self.vote_item.set_sensitive(False)
+#            self.show_notification = False
+#            if nots is True:
+#                if vote_delta.seconds < 40:
+#                    # Reset values to Notify again next time
+#                    self.min30_sent = False
+#                    self.min15_sent = False
+#                    self.min5_sent = False
+#                    self.min1_sent = False
+#                elif vote_delta.seconds > 55 and vote_delta.seconds < 60 and self.min1_sent is False:
+#                    # Send a notification: 1 minute
+#                    Notify.Notification.new("Prepare to vote", "1 minute left!").show()
+#                    self.min30_sent = True
+#                    self.min15_sent = True
+#                    self.min5_sent = True
+#                    self.min1_sent = True
+#                elif vote_delta.seconds > 295 and vote_delta.seconds < 300 and self.min5_sent is False:
+#                    # Send a notification: 5 minutes
+#                    Notify.Notification.new("Prepare to vote", "5 minutes left!").show()
+#                    self.min30_sent = True
+#                    self.min15_sent = True
+#                    self.min5_sent = True
+#                elif vote_delta.seconds > 895 and vote_delta.seconds < 900 and self.min15_sent is False:
+#                    # Send a notification: 15 minutes
+#                    Notify.Notification.new("Prepare to vote", "15 minutes left!").show()
+#                    self.min30_sent = True
+#                    self.min15_sent = True
+#                elif vote_delta.seconds > 1795 and vote_delta.seconds < 1800 and self.min30_sent is False:
+#                    # Send a notification: 30 minutes
+#                    Notify.Notification.new("Prepare to vote", "30 minutes left!").show()
+#                    self.min30_sent = True
+#        return True
     
     def update_data(self):
         if self.ready_to_show is True:
